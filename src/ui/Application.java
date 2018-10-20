4b053d7b032e52df9c79e8ef514188f6ff5808a2
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-20 08:57:28 +0200

    Impl basic application logic

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -3,17 +3,40 @@ package ui;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
     Logger logger = LogManager.getLogger(Application.class);
+    boolean stopRequested = false;
 
     public Application() {
         logger.info("Start application");
-        System.out.println("Hello World!");
+        while (true) {
+            System.out.print("EchoClient> ");
+            processInput();
+            if (stopRequested)
+                break;
+        }
+        logger.info("Stop application");
+    }
+
+    private void processInput() {
+        BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+        String command;
+        try {
+            command = consoleInput.readLine();
+        } catch (IOException e) {
+            logger.warn("EXCEPTION: " + e.getMessage());
+            logger.warn(e.getStackTrace());
+        }
     }
 
     static {
         System.setProperty("log4j.configurationFile", "log4j2.properties.xml");
     }
+
     public static void main(String[] args) {
         new Application();
     }
