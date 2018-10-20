fbb1fb7d4b15dfbbc27740835bd5fd7f932682fb
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-20 08:45:23 +0200

    Setup logging

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -1,7 +1,20 @@
 package ui;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class Application {
-    public static void main(String[] args) {
+    Logger logger = LogManager.getLogger(Application.class);
+
+    public Application() {
+        logger.info("Start application");
         System.out.println("Hello World!");
+    }
+
+    static {
+        System.setProperty("log4j.configurationFile", "log4j2.properties.xml");
+    }
+    public static void main(String[] args) {
+        new Application();
     }
 }
