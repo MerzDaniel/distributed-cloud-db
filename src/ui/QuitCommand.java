afcfd1a3b4ccfcd08df13c010e8a1356e870af52
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-21 13:11:24 +0200

    Disconnect on quit

--- src/ui/QuitCommand.java
+++ src/ui/QuitCommand.java
@@ -4,6 +4,7 @@ public class QuitCommand implements Command {
 
     @Override
     public void execute(ApplicationState state) {
+        new DisconnectCommand().execute(state);
         state.stopRequested = true;
     }
 }
