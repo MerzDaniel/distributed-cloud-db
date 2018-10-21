77a85086c5aefa3c76d603fc7f3453cf19b747b1
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-21 12:05:54 +0200

    Impl read message function and print connection message

--- src/communication/Connection.java
+++ src/communication/Connection.java
@@ -4,17 +4,23 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class Connection {
     Logger logger = LogManager.getLogger(Connection.class);
-
     Socket socket;
+    InputStream in;
 
     public boolean connect(String host, int port) {
         logger.info(String.format("Connect to %s:%d", host, port));
         try {
             socket = new Socket(host, port);
+            in = socket.getInputStream();
+
+            String message = readMessage();
+            System.out.println("Message from server:");
+            System.out.println(message);
         } catch (IOException e) {
             logger.warn(String.format("Connecting to %s:%d failed: %s", host, port, e.getMessage()));
             logger.warn(e.getStackTrace());
@@ -42,4 +48,30 @@ public class Connection {
         }
     }
 
+    /**
+     * Reads a message from the connection
+     *
+     * Handles all errors gracefully and returns empty string on errors.
+     */
+    public String readMessage() {
+        if (!isConnected()) return "";
+        StringBuffer buffer = new StringBuffer();
+        while (true) {
+            try {
+                int val = in.read();
+                if (val == -1) {
+                    logger.info("readLine(): end of stream reached");
+                    break;
+                }
+                char c = (char) val;
+                if (c == '\n') continue;
+                if (c == '\r') break;
+                buffer.append(c);
+            } catch (IOException e) {
+                logger.warn("Exception occured while reading message: "+ e.getMessage());
+                logger.warn(e.getStackTrace());
+            }
+        }
+        return buffer.toString();
+    }
 }
