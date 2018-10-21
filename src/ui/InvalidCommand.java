0507e28852308122ef758db9508435df3f9410b7
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-21 10:51:58 +0200

    Show usage when wrong command is typed

--- src/ui/InvalidCommand.java
+++ src/ui/InvalidCommand.java
@@ -1,9 +1,10 @@
 package ui;
 
 public class InvalidCommand implements Command {
-
     @Override
     public void execute(ApplicationState state) {
-
+        System.out.println("Wrong command.\n");
+        System.out.println("Usage:");
+        System.out.println("exit : Exit application");
     }
 }
