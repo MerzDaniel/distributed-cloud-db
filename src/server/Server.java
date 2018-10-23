d3020b6885b95cd5b0a9356e802b1676c1de229a
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-23 16:18:48 +0200

    Modify project structure

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,7 +1,13 @@
 package server;
 
+import java.net.ServerSocket;
+
 public class Server {
     public static void main(String[] args) {
         System.out.println("Server started");
+
+//        int port = Integer.parseInt(args[0]);
+//        new ServerSocket();
     }
+
 }
