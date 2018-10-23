d1f96bebd6bbeaee952edaf96ad04ce58597cc87
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-23 17:04:41 +0200

    Get Streams from socket

--- src/server/ConnectionHandler.java
+++ src/server/ConnectionHandler.java
@@ -1,11 +1,20 @@
 package server;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import static lib.SocketUtil.tryClose;
+
 public class ConnectionHandler implements Runnable {
     final Socket s;
+    final Logger logger = LogManager.getLogger(ConnectionHandler.class);
+    OutputStream o;
+    InputStream i;
 
     public ConnectionHandler(Socket s) {
         this.s = s;
@@ -14,13 +23,21 @@ public class ConnectionHandler implements Runnable {
     @Override
     public void run() {
         try {
+            i = s.getInputStream();
+            o = s.getOutputStream();
+
             OutputStream o = s.getOutputStream();
             for (char c : "Hello there!".toCharArray()) {
                 o.write((byte) c);
             }
             o.write((byte) '\r');
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.warn("Error during communication with an open connection:"+ e.getMessage());
+            logger.warn(e.getStackTrace());
+        } finally {
+            tryClose(o);
+            tryClose(i);
+            tryClose(s);
         }
     }
 }
