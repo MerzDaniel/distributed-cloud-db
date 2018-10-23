79176649dbb5cfecf293b9127fa30441416f6cf2
Sameera Bandara <sameerahasitha@gmail.com>
2018-10-23 17:07:10 +0200

    add test classes for Unmarshaller and Marshaller

---
+++ test/KVMarshallerTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import junit.framework.TestCase;
+import lib.KVMessage;
+import lib.KVMessageImpl;
+import lib.KVMessageMarshaller;
+import lib.KVMessageUnmarshaller;
+import org.junit.Test;
+
+
+public class KVMarshallerTest extends TestCase {
+
+    @Test
+    public void testMarshall() {
+        KVMessage kvMessage = new KVMessageImpl("Name", "TUM", KVMessage.StatusType.PUT);
+        KVMessageMarshaller kvMessageMarshaller = new KVMessageMarshaller();
+        String unmarshalledMessage = kvMessageMarshaller.marshall(kvMessage);
+
+        assertEquals(unmarshalledMessage, "GET<Name,TUM>");
+    }
+
+}
+
