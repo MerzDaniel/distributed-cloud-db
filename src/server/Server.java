7ae007b022d41a5f46de3cb48044d988237c6c0d
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-23 16:31:45 +0200

    Open server socket and create one thread per connection

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,13 +1,28 @@
 package server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
         System.out.println("Server started");
 
-//        int port = Integer.parseInt(args[0]);
-//        new ServerSocket();
+
+        int port;
+        if (args.length >0) port = Integer.parseInt(args[0]);
+        else port = 50000;
+
+        ServerSocket s;
+        try {
+            s = new ServerSocket(port);
+            while(true) {
+                Socket clientSocket = s.accept();
+                new Thread(new ConnectionHandler(clientSocket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
