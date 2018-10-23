fd8bdf4ad2d924dba67a0d0aed71f9d11961f8c2
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-23 22:31:49 +0200

    Implement ConnectionHandler to Echo messages

--- src/server/ConnectionHandler.java
+++ src/server/ConnectionHandler.java
@@ -1,5 +1,6 @@
 package server;
 
+import lib.SocketUtil;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -13,8 +14,6 @@ import static lib.SocketUtil.tryClose;
 public class ConnectionHandler implements Runnable {
     final Socket s;
     final Logger logger = LogManager.getLogger(ConnectionHandler.class);
-    OutputStream o;
-    InputStream i;
 
     public ConnectionHandler(Socket s) {
         this.s = s;
@@ -22,17 +21,21 @@ public class ConnectionHandler implements Runnable {
 
     @Override
     public void run() {
+        OutputStream o = null;
+        InputStream i = null;
+
         try {
             i = s.getInputStream();
             o = s.getOutputStream();
 
-            OutputStream o = s.getOutputStream();
-            for (char c : "Hello there!".toCharArray()) {
-                o.write((byte) c);
+            SocketUtil.sendMessage(o, "Successfully connected.");
+
+            while (SocketUtil.isConnected(s)) {
+                String msg = SocketUtil.readMessage(i);
+                SocketUtil.sendMessage(o, "ECHO: " + msg);
             }
-            o.write((byte) '\r');
         } catch (IOException e) {
-            logger.warn("Error during communication with an open connection:"+ e.getMessage());
+            logger.warn("Error during communication with an open connection:" + e.getMessage());
             logger.warn(e.getStackTrace());
         } finally {
             tryClose(o);
