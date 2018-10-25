682a8e6a69af22775116d043f78c2483f3111eb4
Sameera Bandara <sameerahasitha@gmail.com>
2018-10-25 20:49:24 +0200

    add getCommand

---
+++ src/client/ui/commands/GetCommand.java
@@ -0,0 +1,41 @@
+package client.ui.commands;
+
+import client.ui.ApplicationState;
+import client.ui.Command;
+import lib.message.KVMessage;
+import lib.message.KVMessageUnmarshaller;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import static client.ui.Util.writeLine;
+
+public class GetCommand implements Command {
+    private String key;
+    private String statusType;
+    private final Logger logger = LogManager.getLogger(GetCommand.class);
+
+    public GetCommand(String statusType, String key){
+        this.statusType = statusType;
+        this.key = key;
+    }
+
+    @Override
+    public void execute(ApplicationState state) {
+        if (!state.connection.isConnected()) {
+            writeLine("Currently not connected to a server");
+            return;
+        }
+
+        state.connection.sendMessage(statusType + "<" + key + ">");
+        String receivedMessage = state.connection.readMessage();
+        KVMessage kVMessageResponse = new KVMessageUnmarshaller().unmarshall(receivedMessage);
+
+        if(kVMessageResponse.isError()){
+            writeLine("An error occurred while executing the GET");
+            logger.error("An error occurred while executing the GET, error=" + statusType);
+        }
+        if(kVMessageResponse.isSuccess()){
+            writeLine(receivedMessage);
+        }
+    }
+}
