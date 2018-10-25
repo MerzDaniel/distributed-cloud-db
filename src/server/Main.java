6ab710e58411fbbd77102910c519e95bbff8bb65
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-25 09:18:36 +0200

    Move Server main to own class

---
+++ src/server/Main.java
@@ -0,0 +1,15 @@
+package server;
+
+public class Main {
+    public static void main(String[] args) {
+        int port;
+        if (args.length >0) port = Integer.parseInt(args[0]);
+        else port = 50000;
+
+        new Server(port).run();
+    }
+
+    static {
+        System.setProperty("log4j.configurationFile", "log4j2-server.properties.xml");
+    }
+}
--- src/server/Server.java
+++ src/server/Server.java
@@ -41,15 +41,4 @@ public class Server implements Runnable{
         }
     }
 
-    public static void main(String[] args) {
-        int port;
-        if (args.length >0) port = Integer.parseInt(args[0]);
-        else port = 50000;
-
-        new Server(port).run();
-    }
-
-    static {
-        System.setProperty("log4j.configurationFile", "log4j2-server.properties.xml");
-    }
 }
