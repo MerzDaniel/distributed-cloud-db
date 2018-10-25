8c51aae79a8024a1d79eadacceaa5e04f81517f5
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-25 14:53:07 +0200

    Implement FIFO cache

---
+++ src/server/kv/FifoCachedKeyValueStore.java
@@ -0,0 +1,52 @@
+package server.kv;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FifoCachedKeyValueStore extends CachedKeyValueStore {
+    class CacheEntry {
+        public CacheEntry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String key;
+        public String value;
+    }
+
+    List<String> cachePriority = new LinkedList<>();
+    Dictionary<String, CacheEntry> cache = new Hashtable<>();
+
+    public FifoCachedKeyValueStore(int cacheSize) {
+        super(cacheSize);
+    }
+
+    public FifoCachedKeyValueStore(int cacheSize, KeyValueStore store) {
+        super(cacheSize, store);
+    }
+
+    @Override
+    protected void addToCache(String key, String value) {
+        if (isCached(key)) return;
+        if (cache.size() >= this.cacheSize) {
+            cache.remove(cachePriority.get(0));
+            cachePriority.remove(0);
+        }
+        cachePriority.add(key);
+        cache.put(key, new CacheEntry(key, value));
+    }
+
+    @Override
+    protected boolean isCached(String key) {
+        return cache.get(key) == null;
+    }
+
+    @Override
+    protected String getFromCache(String key) throws KeyNotFoundException {
+        CacheEntry entry = cache.get(key);
+        if (entry == null) throw new KeyNotFoundException();
+        return entry.value;
+    }
+}
