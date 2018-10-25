eb2462e6b95f5b1db26946293831d7d82286d8f1
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-25 11:11:17 +0200

    Test: put() and hasKey() in SimpleKeyValueStore

--- test/server/kv/SimpleKeyValueStoreTest.java
+++ test/server/kv/SimpleKeyValueStoreTest.java
@@ -30,6 +30,16 @@ public class SimpleKeyValueStoreTest {
         assertEquals("c", sv.get("ab"));
     }
 
+    @Test
+    public void shouldReturnCorrectValuesForHasKey() throws KeyNotFoundException, DbError {
+        Reader reader = new StringReader("ab=c\nde=fg\na=ha");
+        Writer writer = new StringWriter();
+        KeyValueStore sv = new SimpleKeyValueStore(reader, writer);
+
+        assertEquals(true, sv.hasKey("de"));
+        assertEquals(false, sv.hasKey("non-existing-key"));
+    }
+
     @Test(expected = KeyNotFoundException.class)
     public void shouldThrowKeyNotFound() throws KeyNotFoundException, DbError {
         Reader reader = new StringReader("ab=c\nde=fg\na=ha");
@@ -37,5 +47,15 @@ public class SimpleKeyValueStoreTest {
         KeyValueStore sv = new SimpleKeyValueStore(reader, writer);
 
         sv.get("non-existing-key");
+    }
+
+    @Test
+    public void shouldWriteValues() throws DbError {
+        Reader reader = new StringReader("");
+        Writer writer = new StringWriter();
+        KeyValueStore sv = new SimpleKeyValueStore(reader, writer);
+
+        sv.put("key", "value");
+        assertEquals("key=value\n", ((StringWriter) writer).getBuffer().toString());
     }
 }
