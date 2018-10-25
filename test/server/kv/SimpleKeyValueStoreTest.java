0a976cbb7623d900cbc838130eba05e154074b46
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-25 10:42:43 +0200

    Test: KeyValueStore Get

---
+++ test/server/kv/SimpleKeyValueStoreTest.java
@@ -0,0 +1,32 @@
+package server.kv;
+
+import org.junit.Test;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import static junit.framework.Assert.assertEquals;
+
+public class SimpleKeyValueStoreTest {
+    @Test
+    public void shouldGetValue() throws KeyNotFoundException, DbError {
+        Reader reader = new StringReader("ab=c\nde=fg\na=ha");
+        Writer writer = new StringWriter();
+        KeyValueStore sv = new SimpleKeyValueStore(reader, writer);
+
+        assertEquals("fg", sv.get("de"));
+    }
+
+    @Test
+    public void shouldGetMultipleValues() throws KeyNotFoundException, DbError {
+        Reader reader = new StringReader("ab=c\nde=fg\na=ha");
+        Writer writer = new StringWriter();
+        KeyValueStore sv = new SimpleKeyValueStore(reader, writer);
+
+        assertEquals("fg", sv.get("de"));
+        assertEquals("ha", sv.get("a"));
+        assertEquals("c", sv.get("ab"));
+    }
+}
