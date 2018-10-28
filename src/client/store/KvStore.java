55292b77fbd37f4d9182300c203b558788fb5f6a
Sameera Bandara <sameerahasitha@gmail.com>
2018-10-28 13:27:08 +0100

    KvStore implemenation

--- src/client/store/KvStore.java
+++ src/client/store/KvStore.java
@@ -1,21 +1,67 @@
 package client.store;
 
-import lib.message.KVMessage;
+import client.communication.Connection;
+import lib.message.*;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import static client.ui.Util.writeLine;
 
 public class KvStore {
     final String host;
     final int port;
+    final Connection connection;
+
+    private final Logger logger = LogManager.getLogger(KvStore.class);
+
     public KvStore(String host, int port) {
         this.host = host;
         this.port = port;
+        this.connection = new Connection();
     }
 
-    public void connect() {
-
+    public boolean connect() {
+        return this.connection.connect(host, port);
     }
+
     public void disconnect() {
-
+        if (!connection.isConnected()) {
+            writeLine("Currently not connected to a server");
+            return;
+        }
+        this.connection.disconnect();
     }
-    public KVMessage get(String key) {return null;}
-    public KVMessage put(String key, String value) {return null;}
+
+    public KVMessage get(String key) {
+        if (!connection.isConnected()) {
+            writeLine("Currently not connected to a server");
+            return new KVMessageImpl("", "", KVMessage.StatusType.GET_ERROR);
+        }
+
+        KVMessage kvMessageRequest = new KVMessageImpl(key, null, KVMessage.StatusType.GET);
+        this.connection.sendMessage(KVMessageMarshaller.marshall(kvMessageRequest));
+        String response = this.connection.readMessage();
+        try {
+            return KVMessageUnmarshaller.unmarshall(response);
+        } catch (UnmarshallException e) {
+            logger.warn("Invalid response from the server.");
+            writeLine("Response from the server was invalid.");
+            //todo
+            return new KVMessageImpl(null, null, KVMessage.StatusType.INVALID_MESSAGE);
+        }
+    }
+
+    public KVMessage put(String key, String value) {
+        KVMessage kvMessageRequest = new KVMessageImpl(key, value, KVMessage.StatusType.PUT);
+        this.connection.sendMessage(KVMessageMarshaller.marshall(kvMessageRequest));
+        String response = this.connection.readMessage();
+        try {
+            return KVMessageUnmarshaller.unmarshall(response);
+        } catch (UnmarshallException e) {
+            logger.warn("Got an invalid message.");
+            writeLine("The response of the server was invalid");
+            //todo
+            return new KVMessageImpl(null, null, KVMessage.StatusType.INVALID_MESSAGE);
+        }
+    }
 }
