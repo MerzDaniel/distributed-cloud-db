a988229735523df6e3a3116905646dfca56436d5
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-28 13:22:05 +0100

    Delete messages in MessageFactory

--- src/lib/message/MessageFactory.java
+++ src/lib/message/MessageFactory.java
@@ -4,8 +4,8 @@ public class MessageFactory {
     public static KVMessage createGetMessage(String key) {
         return new KVMessageImpl(key, null, KVMessage.StatusType.GET);
     }
-    public static KVMessage createGetSuccessMessage() {
-        return new KVMessageImpl(KVMessage.StatusType.GET_SUCCESS);
+    public static KVMessage createGetSuccessMessage(String key, String value) {
+        return new KVMessageImpl(key, value, KVMessage.StatusType.GET_SUCCESS);
     }
     public static KVMessage createGetErrorMessage() {
         return new KVMessageImpl(KVMessage.StatusType.GET_ERROR);
@@ -18,6 +18,15 @@ public class MessageFactory {
     }
     public static KVMessage createPutErrorMessage() {
         return new KVMessageImpl(KVMessage.StatusType.PUT_ERROR);
+    }
+    public static KVMessage createDeleteMessage(String key, String value) {
+        return new KVMessageImpl(key, value, KVMessage.StatusType.DELETE);
+    }
+    public static KVMessage createDeleteSuccessMessage() {
+        return new KVMessageImpl(KVMessage.StatusType.DELETE_SUCCESS);
+    }
+    public static KVMessage createDeleteErrorMessage() {
+        return new KVMessageImpl(KVMessage.StatusType.DELETE_ERROR);
     }
     public static KVMessage createInvalidMessage() {
         return new KVMessageImpl(KVMessage.StatusType.INVALID_MESSAGE);
