d4feabf57af62fd1799aea17e8032919c9cc7f11
Sameera Bandara <sameerahasitha@gmail.com>
2018-10-28 12:08:19 +0100

    modify help command with get and put

--- src/client/ui/commands/HelpCommand.java
+++ src/client/ui/commands/HelpCommand.java
@@ -9,11 +9,13 @@ public class HelpCommand implements Command {
     @Override
     public void execute(ApplicationState state) {
         writeLine("Usage:");
-        writeLine("connect <host> <port> : Connect to a host");
-        writeLine("send <msg>            : Send message to connected host");
-        writeLine("logLevel <level>      : Set loglevel. Supported logLevels are ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF");
-        writeLine("disconnect            : Disconnects from the connected host");
-        writeLine("help                  : Print this help text");
-        writeLine("quit                  : Exit application");
+        writeLine("connect <host> <port>    : Connect to a host");
+        writeLine("send <msg>               : Send message to connected host");
+        writeLine("put <key> <value>        : Put <key,value> to the data store");
+        writeLine("get <key>                : Get <value> for the <key> from the data store");
+        writeLine("logLevel <level>         : Set loglevel. Supported logLevels are ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF");
+        writeLine("disconnect               : Disconnects from the connected host");
+        writeLine("help                     : Print this help text");
+        writeLine("quit                     : Exit application");
     }
 }
