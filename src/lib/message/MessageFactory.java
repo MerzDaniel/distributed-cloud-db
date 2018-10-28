766c8fd48a4eacdf19581b3d205e7d6c0894587d
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-28 13:17:33 +0100

    Extend Message Factory

--- src/lib/message/MessageFactory.java
+++ src/lib/message/MessageFactory.java
@@ -4,22 +4,22 @@ public class MessageFactory {
     public static KVMessage createGetMessage(String key) {
         return new KVMessageImpl(key, null, KVMessage.StatusType.GET);
     }
-    public static KVMessage createGetSuccessMessage(String key, String value) {
+    public static KVMessage createGetSuccessMessage() {
         return new KVMessageImpl(KVMessage.StatusType.GET_SUCCESS);
     }
-    public static KVMessage createGetErrorMessage(String key, String value) {
+    public static KVMessage createGetErrorMessage() {
         return new KVMessageImpl(KVMessage.StatusType.GET_ERROR);
     }
     public static KVMessage createPutMessage(String key, String value) {
         return new KVMessageImpl(key, value, KVMessage.StatusType.PUT);
     }
-    public static KVMessage createPutSuccessMessage(String key, String value) {
+    public static KVMessage createPutSuccessMessage() {
         return new KVMessageImpl(KVMessage.StatusType.PUT_SUCCESS);
     }
-    public static KVMessage createPutErrorMessage(String key, String value) {
+    public static KVMessage createPutErrorMessage() {
         return new KVMessageImpl(KVMessage.StatusType.PUT_ERROR);
     }
-    public static KVMessage createInvalidMessage(String key, String value) {
+    public static KVMessage createInvalidMessage() {
         return new KVMessageImpl(KVMessage.StatusType.INVALID_MESSAGE);
     }
 }
