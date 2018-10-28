077c877f7d506b028d5ba156d816966468ceb3b0
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-28 13:16:48 +0100

    Extend Message Factory

--- src/lib/message/MessageFactory.java
+++ src/lib/message/MessageFactory.java
@@ -4,7 +4,22 @@ public class MessageFactory {
     public static KVMessage createGetMessage(String key) {
         return new KVMessageImpl(key, null, KVMessage.StatusType.GET);
     }
+    public static KVMessage createGetSuccessMessage(String key, String value) {
+        return new KVMessageImpl(KVMessage.StatusType.GET_SUCCESS);
+    }
+    public static KVMessage createGetErrorMessage(String key, String value) {
+        return new KVMessageImpl(KVMessage.StatusType.GET_ERROR);
+    }
     public static KVMessage createPutMessage(String key, String value) {
         return new KVMessageImpl(key, value, KVMessage.StatusType.PUT);
+    }
+    public static KVMessage createPutSuccessMessage(String key, String value) {
+        return new KVMessageImpl(KVMessage.StatusType.PUT_SUCCESS);
+    }
+    public static KVMessage createPutErrorMessage(String key, String value) {
+        return new KVMessageImpl(KVMessage.StatusType.PUT_ERROR);
+    }
+    public static KVMessage createInvalidMessage(String key, String value) {
+        return new KVMessageImpl(KVMessage.StatusType.INVALID_MESSAGE);
     }
 }
