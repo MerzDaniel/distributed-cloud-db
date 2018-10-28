1170fd69a67770785d99c5697a0dff24d21a860b
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-28 16:29:25 +0100

    Handle GET and PUT messages using db

--- src/server/ConnectionHandler.java
+++ src/server/ConnectionHandler.java
@@ -4,8 +4,10 @@ import lib.SocketUtil;
 import lib.message.*;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import server.kv.DbError;
+import server.kv.KeyNotFoundException;
+import server.kv.KeyValueStore;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -14,10 +16,12 @@ import static lib.SocketUtil.tryClose;
 
 public class ConnectionHandler implements Runnable {
     final Socket s;
+    private KeyValueStore db;
     final Logger logger = LogManager.getLogger(ConnectionHandler.class);
 
-    public ConnectionHandler(Socket s) {
+    public ConnectionHandler(Socket s, KeyValueStore db) {
         this.s = s;
+        this.db = db;
     }
 
     @Override
@@ -37,15 +41,31 @@ public class ConnectionHandler implements Runnable {
                 try {
                     KVMessage kvMessage = null;
                     kvMessage = KVMessageUnmarshaller.unmarshall(msg);
-                    logger.debug("Got a message: " + kvMessage.getStatus());
+                    logger.debug("Got a message: %s <%s,%s>" + kvMessage.getStatus());
 
                     KVMessage response;
                     switch (kvMessage.getStatus()) {
                         case GET:
-                            response = MessageFactory.createGetSuccessMessage(kvMessage.getKey(), "value");
+                            try {
+                                String value = db.get(kvMessage.getKey());
+                                response = MessageFactory.createGetSuccessMessage(kvMessage.getKey(), value);
+                            } catch (KeyNotFoundException e) {
+                                logger.info(String.format("Key '%s' not found", kvMessage.getKey()));
+                                response = MessageFactory.createGetErrorMessage();
+                            } catch (DbError e) {
+                                logger.warn("Some error occured at database level", e);
+                                response = MessageFactory.createGetErrorMessage();
+                            }
                             break;
                         case PUT:
-                            response = MessageFactory.createPutSuccessMessage();
+                            logger.debug(String.format("New PUT message from client: <%s,%s>", kvMessage.getKey(), kvMessage.getValue()));
+                            try {
+                                db.put(kvMessage.getKey(), kvMessage.getValue());
+                                response = MessageFactory.createPutSuccessMessage();
+                            } catch (DbError e) {
+                                logger.warn("PUT: Databaseerror!", e);
+                                response = MessageFactory.createPutErrorMessage();
+                            }
                             break;
                         case DELETE:
                             response = MessageFactory.createDeleteSuccessMessage();
@@ -63,8 +83,7 @@ public class ConnectionHandler implements Runnable {
                 }
             }
         } catch (Exception e) {
-            logger.warn("Error during communication with an open connection:" + e.getMessage());
-            logger.warn(e.getStackTrace());
+            logger.warn("Error during communication with an open connection:" + e.getMessage(), e);
         } finally {
             tryClose(o);
             tryClose(i);
