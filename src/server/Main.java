09b78b5ceccf6717b76d3661edca55758f3aa41c
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-10-30 15:58:49 +0100

    cache type and size Command line options for server

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,6 +1,7 @@
 package server;
 
 import org.apache.commons.cli.*;
+import server.kv.CacheType;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,24 +9,45 @@ public class Main {
         Options options = new Options();
         options.addOption(new Option("h", "help", false, "show help text"));
         options.addOption(new Option("p", "port", true, "port of the server"));
+        options.addOption(new Option(
+                "c", "cache-type", true,
+                "Cachetype to use. possible values: FIFO,LRU,LFU,NONE(default)"
+        ));
+        options.addOption(new Option(
+                "s", "cache-size", true,
+                "Cache size to use (number of key,value pairs that will be hold in cache)"
+        ));
 
         CommandLine cmd = null;
-        boolean showHelp = false;
         try {
             cmd = c.parse(options, args);
         } catch (ParseException e) {
-            showHelp = true;
             System.out.println("Wrong argument.");
+            printHelp(options);
+            System.exit(1);
         }
 
-        if (showHelp || cmd.hasOption("h")) {
-            new HelpFormatter().printHelp("java -jar server.jar", options);
+        if (cmd.hasOption("h")) {
+            printHelp(options);
             System.exit(0);
         }
 
         int port = Integer.parseInt(cmd.getOptionValue("p", "50000"));
-        ;
-        new Server(port).run();
+        int cacheSize = Integer.parseInt(cmd.getOptionValue("s", "10"));
+        CacheType type = CacheType.NONE;
+        try {
+            type = CacheType.valueOf(cmd.getOptionValue("c", "NONE"));
+        } catch(IllegalArgumentException e) {
+            System.out.println("Wrong argument.");
+            printHelp(options);
+            System.exit(1);
+        }
+
+        new Server(port, cacheSize, type).run();
+    }
+
+    private static void printHelp(Options options) {
+        new HelpFormatter().printHelp("java -jar server.jar", options);
     }
 
     static {
