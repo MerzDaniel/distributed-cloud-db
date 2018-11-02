c3d9b6cdc6a813b3fa79f8e3959706e87bb4ed13
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-02 22:44:42 +0100

    add deleteKey functionality and also fix the issue of key not updated properly when the key is already present

--- src/server/kv/SimpleKeyValueStore.java
+++ src/server/kv/SimpleKeyValueStore.java
@@ -13,6 +13,7 @@ public class SimpleKeyValueStore implements KeyValueStore {
 
     final static File DB_DIRECTORY = new File(Paths.get("db").toUri());
     final static File DB_FILE = new File(Paths.get(DB_DIRECTORY.toString(), "db").toUri());
+    final static File TEMP_DB_FILE = new File(Paths.get(DB_DIRECTORY.toString(), "temp_db").toUri());
 
     Reader reader;
     Writer writer;
@@ -81,8 +82,18 @@ public class SimpleKeyValueStore implements KeyValueStore {
     @Override
     public void put(String key, String value) throws DbError {
         try {
+            ioDelete(key);
+        } catch (KeyNotFoundException e) {
+            logger.debug(String.format("Key %s is not present in the database", key));
+        } catch (IOException e) {
+            logger.error(String.format("An error occurred trying delete any existing keys", e.getLocalizedMessage()));
+            throw new DbError(e);
+        }
+
+        try {
             ioPut(key, value);
         } catch (IOException e) {
+            logger.error("IO Exception during PUT", e);
             throw new DbError(e);
         }
     }
@@ -104,7 +115,57 @@ public class SimpleKeyValueStore implements KeyValueStore {
 
     @Override
     public boolean deleteKey(String key) throws DbError {
-        return false;
+        try {
+            ioDelete(key);
+        } catch (IOException | KeyNotFoundException e) {
+            logger.error("IO Exception during DELETE", e);
+            throw new DbError(e);
+        }
+        return true;
+    }
+
+    private String ioDelete(String key) throws IOException, KeyNotFoundException {
+        Reader localReader = new FileReader(DB_FILE);
+        Writer localWriter = new FileWriter(TEMP_DB_FILE);
+
+        if (localReader.markSupported()) {
+            logger.debug("Reset marker in FileReader.");
+            localReader.reset();
+        } else {
+            logger.warn("Reset not supported!");
+        }
+
+        int rows = 0;
+        boolean keyFound = false;
+        try (final BufferedReader bufferedReader = new BufferedReader(localReader); final BufferedWriter bufferedWriter = new BufferedWriter(localWriter)) {
+            for (Iterator<String> it = bufferedReader.lines().iterator(); it.hasNext(); ) {
+                String line = it.next();
+                String[] split = line.split("=");
+                if (split.length != 2) continue;
+                if (!split[0].equals(key)) {
+                    bufferedWriter.write(line + "\n");
+                } else {
+                    keyFound = true;
+                }
+                rows++;
+            }
+        } catch (IOException e) {
+            throw e;
+        }
+
+        boolean renameSuccess = TEMP_DB_FILE .renameTo(DB_FILE);
+        if (!renameSuccess) {
+            throw new IOException("An error occurred while renaming the database");
+        }
+        writer.close();
+        writer = new FileWriter(DB_FILE, true);
+
+        if (keyFound) {
+            return key;
+        }
+
+        logger.debug(String.format("Parsed %drows without finding the key %s", rows, key));
+        throw new KeyNotFoundException();
     }
 
 }
