371b793739378ad2073ecc4b07337c0be52321c6
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-04 20:53:09 +0100

    add JavaDoc comments

--- src/server/kv/cache/LRUCachedKeyValueStore.java
+++ src/server/kv/cache/LRUCachedKeyValueStore.java
@@ -28,6 +28,11 @@ public class LRUCachedKeyValueStore extends CachedKeyValueStore {
         super(cacheSize, store);
     }
 
+    /**
+     * add the given {@code key} and {@code value} into the database
+     * @param key the {@code key}
+     * @param value the {@code value}
+     */
     @Override
     protected void addToCache(String key, String value) {
         if (isCached(key)) return;
@@ -40,11 +45,22 @@ public class LRUCachedKeyValueStore extends CachedKeyValueStore {
         cache.put(key, new CacheEntry(key, value));
     }
 
+    /**
+     * returns whether the {@code key} is present or not in the database
+     * @param key the {@code key} to be checked
+     * @return whether the {@code key} is present or not
+     */
     @Override
     protected boolean isCached(String key) {
         return cache.get(key) != null;
     }
 
+    /**
+     * returns the value associated with the {@code key} in the database
+     * @param key the {@code key} for which the value is required
+     * @return the value associated with the {@code key}
+     * @throws KeyNotFoundException if any errors occurred while retrieving the value
+     */
     @Override
     protected String getFromCache(String key) throws KeyNotFoundException {
         CacheEntry entry = cache.get(key);
