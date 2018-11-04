05725c022c32341666edb18bf8df0f38ed333ede
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-04 22:10:52 +0100

    add JavaDoc comments

--- src/lib/message/KVMessageImpl.java
+++ src/lib/message/KVMessageImpl.java
@@ -1,5 +1,8 @@
 package lib.message;
 
+/**
+ * The representation of the data passed between the client and the server
+ */
 public class KVMessageImpl implements KVMessage {
     private final String key;
     private final String value;
@@ -16,21 +19,37 @@ public class KVMessageImpl implements KVMessage {
         this.statusType = statusType;
     }
 
+    /**
+     * @return the key that is associated with this message,
+     * null if not key is associated.
+     */
     @Override
     public String getKey() {
         return key;
     }
 
+    /**
+     * @return the value that is associated with this message,
+     * null if not value is associated.
+     */
     @Override
     public String getValue() {
         return value;
     }
 
+    /**
+     * @return a status string that is used to identify request types,
+     * response types and error types associated to the message.
+     */
     @Override
     public StatusType getStatus() {
         return statusType;
     }
 
+    /**
+     * Returns whether the message is an error
+     * @return true if this message has an error status
+     */
     @Override
     public boolean isError() {
         switch (this.statusType) {
@@ -47,6 +66,10 @@ public class KVMessageImpl implements KVMessage {
         }
     }
 
+    /**
+     * Returns whether this message is a success
+     * @return true if this message has an success status
+     */
     @Override
     public boolean isSuccess() {
         switch (this.statusType) {
