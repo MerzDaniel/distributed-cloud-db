1a20908e4452d0f6daf652955504f600402f928a
ga74wim <ga74wim@mytum.de>
2018-11-04 22:42:53 +0100

    Delete InValidKeyValueLengthException.java

--- src/lib/message/InValidKeyValueLengthException.java
+++
@@ -1,7 +0,0 @@
-package lib.message;
-
-public class InValidKeyValueLengthException extends Exception {
-    public InValidKeyValueLengthException(String message) {
-        super(message);
-    }
-}
