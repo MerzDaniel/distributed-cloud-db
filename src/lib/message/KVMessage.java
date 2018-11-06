f201866618e9ab49811a9e69a5f747d898db7d62
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-06 17:52:10 +0100

    add new KVMessage types

--- src/lib/message/KVMessage.java
+++ src/lib/message/KVMessage.java
@@ -6,19 +6,22 @@ package lib.message;
 public interface KVMessage {
 
     public enum StatusType {
-        GET, 			/* Get - request */
-        GET_NOT_FOUND, 	/* requested tuple (i.e. value) not found */
-        GET_ERROR, 		/* generic Get error */
-        GET_SUCCESS, 	/* requested tuple (i.e. value) found */
-        PUT, 			/* Put - request */
-        PUT_SUCCESS, 	/* Put - request successful, tuple inserted */
-        PUT_UPDATE, 	/* Put - request successful, i.e. value updated */
-        PUT_ERROR, 		/* Put - request not successful */
-        DELETE, 		/* Delete - request */
-        DELETE_SUCCESS, /* Delete - request successful */
-        DELETE_ERROR, 	/* Delete - request successful */
-        INVALID_MESSAGE,/* Invalid message */
-        CONNECT_SUCCESSFUL, /* Connection successful message */
+        GET, 			        /* Get - request */
+        GET_NOT_FOUND, 	        /* requested tuple (i.e. value) not found */
+        GET_ERROR, 		        /* generic Get error */
+        GET_SUCCESS, 	        /* requested tuple (i.e. value) found */
+        PUT, 			        /* Put - request */
+        PUT_SUCCESS, 	        /* Put - request successful, tuple inserted */
+        PUT_UPDATE, 	        /* Put - request successful, i.e. value updated */
+        PUT_ERROR, 		        /* Put - request not successful */
+        DELETE, 		        /* Delete - request */
+        DELETE_SUCCESS,         /* Delete - request successful */
+        DELETE_ERROR, 	        /* Delete - request successful */
+        INVALID_MESSAGE,        /* Invalid message */
+        CONNECT_SUCCESSFUL,     /* Connection successful message */
+        SERVER_STOPPED,         /* Server is stopped, no requests are processed */
+        SERVER_WRITE_LOCK,      /* Server locked for out, only get possible */
+        SERVER_NOT_RESPONSIBLE  /* Request not successful, server not
     }
 
     /**
