3f53e12e3a92617a8807274a9a23dd1bf4853b49
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-11-06 18:42:13 +0100

    Add logging to ecs

--- src/ecs/EcsAdminConsole.java
+++ src/ecs/EcsAdminConsole.java
@@ -1,5 +1,8 @@
 package ecs;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import java.io.*;
 import java.util.Iterator;
 
@@ -7,12 +10,15 @@ public class EcsAdminConsole {
     private State state = new State();
     private File configPath;
     private boolean stopRequested = false;
+    private Logger logger = LogManager.getLogger(EcsAdminConsole.class);
 
     public EcsAdminConsole(String configPath) {
         this.configPath = new File(configPath);
     }
 
     public void start() throws IOException {
+        logger.info("Started");
+
         System.out.println("Starting ECS client console.");
         loadConfiguration();
 
@@ -27,6 +33,7 @@ public class EcsAdminConsole {
 
     private void loadConfiguration() throws IOException {
         if (!configPath.exists()) {
+            logger.debug("Create new config file");
             System.out.println("Config file does not exist. Creating a new one at '" + configPath + "' ...");
             configPath.getParentFile().mkdirs();
             configPath.createNewFile();
@@ -36,7 +43,14 @@ public class EcsAdminConsole {
         try (BufferedReader reader = new BufferedReader(new FileReader(configPath))) {
             for (Iterator<String> it = reader.lines().iterator(); it.hasNext(); ) {
                 String line = it.next();
+                String[] tokens = line.split(" ");
+                if (tokens.length != 3) {
+                    logger.error("Wrong layout in config file: '" + line + "'");
+                    System.out.println("Config file layout is wrong. Should be '<name> <host> <port>'");
+                    System.exit(0);
+                }
 
+                // todo: add new server to the table in the state
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
--- src/ecs/Main.java
+++ src/ecs/Main.java
@@ -10,4 +10,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         new EcsAdminConsole(defaultConfigPath).start();
     }
+
+    static {
+        System.setProperty("log4j.configurationFile", "log4j2-ecs.properties.xml");
+    }
 }
