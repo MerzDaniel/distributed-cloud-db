a82a571d7632a0d1de9d24a879b60c08a10de4d3
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-06 18:05:51 +0100

    add missing comment block from slides

--- src/lib/message/KVMessage.java
+++ src/lib/message/KVMessage.java
@@ -21,7 +21,7 @@ public interface KVMessage {
         CONNECT_SUCCESSFUL,     /* Connection successful message */
         SERVER_STOPPED,         /* Server is stopped, no requests are processed */
         SERVER_WRITE_LOCK,      /* Server locked for out, only get possible */
-        SERVER_NOT_RESPONSIBLE  /* Request not successful, server not */
+        SERVER_NOT_RESPONSIBLE  /* Request not successful, server not responsible for key */
     }
 
     /**
