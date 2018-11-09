b145bd31e165b0b4efca98baf31f1ea562a5cdf8
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-09 18:50:29 +0100

    add marshalling/unmarshalling methods

--- src/lib/server/metadata/KVStoreMetaDataUtil.java
+++ src/lib/server/metadata/KVStoreMetaDataUtil.java
@@ -1,8 +1,12 @@
 package lib.server.metadata;
 
+import lib.message.UnmarshallException;
 import lib.server.KVServerNotFoundException;
 import lib.server.metadata.KVStoreMetaData;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -18,5 +22,40 @@ public class KVStoreMetaDataUtil {
         }
 
         return kvServer.get();
+    }
+
+    public static String marshallKvStoreMetaData(KVStoreMetaData kvStoreMetaData) {
+        final String RECORD_SEPARATOR = "\u001E";
+        return kvStoreMetaData.getKvServerList().stream().map(it -> KVStoreMetaDataUtil.marshallKvServerMetaData(it)).collect(Collectors.joining(RECORD_SEPARATOR));
+    }
+
+    private static String marshallKvServerMetaData(KVStoreMetaData.KVServerMetaData kvServerMetaData) {
+        final String ELEMENT_SEPARATOR = "\u001F";
+        return kvServerMetaData.host + "" + kvServerMetaData.port + ELEMENT_SEPARATOR + kvServerMetaData.fromHash + ELEMENT_SEPARATOR + kvServerMetaData.toHash;
+    }
+
+    public static KVStoreMetaData unmarshallKVStoreMetaData(String kvStoreMetaData) throws UnmarshallException {
+        final String RECORD_SEPARATOR = "\u001E";
+
+        List<KVStoreMetaData.KVServerMetaData> kvServerMetaDataList = new ArrayList<>();
+        String[] kvServers = kvStoreMetaData.split(RECORD_SEPARATOR);
+
+        for (String kvServer : kvServers) {
+            KVStoreMetaData.KVServerMetaData kvServerMetaData = KVStoreMetaDataUtil.unmarshallKvServerMetaData(kvServer);
+            kvServerMetaDataList.add(kvServerMetaData);
+        }
+
+        return new KVStoreMetaData(kvServerMetaDataList);
+    }
+
+    private static KVStoreMetaData.KVServerMetaData unmarshallKvServerMetaData(String kvServerMetaData) throws UnmarshallException {
+        final String ELEMENT_SEPARATOR = "\u001F";
+
+        try {
+            String[] split = kvServerMetaData.split(ELEMENT_SEPARATOR);
+            return new KVStoreMetaData.KVServerMetaData(split[0], split[1], Integer.valueOf(split[2]), Integer.valueOf(split[3]));
+        } catch (Exception ex) {
+            throw new UnmarshallException(ex);
+        }
     }
 }
