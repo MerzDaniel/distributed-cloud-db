a45e2ac60f0ba51c9c009c6c91f26cf2f8ea7c0b
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-11 23:08:00 +0100

    add connect error type

--- src/lib/message/KVMessage.java
+++ src/lib/message/KVMessage.java
@@ -19,6 +19,7 @@ public interface KVMessage extends IMessage {
         DELETE_ERROR, 	        /* Delete - request successful */
         INVALID_MESSAGE,        /* Invalid message */
         CONNECT_SUCCESSFUL,     /* Connection successful message */
+        CONNECT_ERROR,         /* Connect error message */
         SERVER_STOPPED,         /* Server is stopped, no requests are processed */
         SERVER_WRITE_LOCK,      /* Server locked for out, only get possible */
         SERVER_NOT_RESPONSIBLE  /* Request not successful, server not responsible for key */
