5dde0eead2212a45931493504a3fab69605f5a49
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-11-11 16:17:25 +0100

    Update admin message types

--- src/lib/message/KVAdminMessage.java
+++ src/lib/message/KVAdminMessage.java
@@ -1,17 +1,20 @@
 package lib.message;
 
+import lib.metadata.KVStoreMetaData;
+import lib.metadata.MetaContent;
+
 /**
  * This is the absruct structure for the messages between KVServer and ECS
  */
 public class KVAdminMessage implements IMessage {
-    private final String host;
-    private final int port;
-    private final StatusType status;
+    public final StatusType status;
+    public KVStoreMetaData meta;
+    public MetaContent metaContent;
 
     enum StatusType {
-        INIT, 			        /* Initialize a KVServer */
-        INIT_SUCCESS, 	        /* Initializing the KVServer is successfull */
-        INIT_ERROR, 		    /* Initializing the KVServer is not successful */
+        CONFIGURE,
+        CONFIGURE_SUCCESS,
+        CONFIGURE_ERROR,
         START, 			        /* Start a KVServer */
         START_SUCCESS, 	        /* Starting the KVServer is success */
         START_ERROR, 		    /* Starting the KVServer is not success  */
@@ -21,48 +24,17 @@ public class KVAdminMessage implements IMessage {
         SHUT_DOWN, 		        /* Shut down a KVServer */
         SHUT_DOWN_SUCCESS,      /* Shutting down the KVServer is success */
         SHUT_DOWN_ERROR, 	    /* Shutting down the KVServer is not success */
-        ADD, 		            /* Add a new KVServer */
-        ADD_SUCCESS,            /* Adding the KVServer is success */
-        ADD_ERROR, 	            /* Adding the KVServer is not success */
-        REMOVE, 		        /* Remove a KVServer */
-        REMOVE_SUCCESS,         /* Removing the KVServer is success */
-        REMOVE_ERROR, 	        /* Removing down the KVServer is not success */
+        /** Move data */
+        MOVE,
+        MOVE_SUCCESS,
     }
 
     /**
      * Create a ECSMesage
      *
-     * @param host host of the message
-     * @param port port of the message
      * @param status status of the message
      */
-    public KVAdminMessage(String host, int port, StatusType status) {
-        this.host = host;
-        this.port = port;
+    public KVAdminMessage(StatusType status) {
         this.status = status;
-    }
-
-    /**
-     *
-     * @return the statusType associated with this message
-     */
-    public StatusType getStatus() {
-        return this.status;
-    }
-
-    /**
-     *
-     * @return the host associated with this message
-     */
-    public String getHost() {
-        return this.host;
-    }
-
-    /**
-     *
-     * @return the port associated with this message
-     */
-    public int getPort() {
-        return this.port;
     }
 }
