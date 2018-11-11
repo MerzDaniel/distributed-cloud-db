f4686c536419419b4511f95d8b11d77b74d6f1ad
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-11 22:47:24 +0100

    move getKVServer method to KVStoreMetaData class

--- src/lib/metadata/KVStoreMetaData.java
+++ src/lib/metadata/KVStoreMetaData.java
@@ -1,9 +1,13 @@
 package lib.metadata;
 
+import lib.hash.HashUtil;
 import lib.message.MarshallingException;
 
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class KVStoreMetaData {
@@ -37,6 +41,20 @@ public class KVStoreMetaData {
         }
 
         return new KVStoreMetaData(metaContentList);
+    }
+
+    public MetaContent getKVServer(String key) throws NoSuchAlgorithmException, KVServerNotFoundException {
+        BigInteger hash = HashUtil.getHash(key);
+
+        if (kvServerList.size() == 1) return kvServerList.get(0);
+
+        for (int i = 0; i < kvServerList.size(); i++) {
+            if (kvServerList.get(i).getFromHash().compareTo(hash) == -1
+                    && hash.compareTo(kvServerList.get(i + 1 % kvServerList.size()).getFromHash()) == -1)
+                return kvServerList.get(i);
+        }
+
+        throw new KVServerNotFoundException();
     }
 
 }
