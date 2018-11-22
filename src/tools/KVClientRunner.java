60b443f77ad91ab4f945945e984cfb81e9daba0b
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-22 17:33:43 +0100

    load test data

--- src/tools/KVClientRunner.java
+++ src/tools/KVClientRunner.java
@@ -8,6 +8,7 @@ import client.ui.commands.GetCommand;
 import client.ui.commands.PutCommand;
 import lib.StreamUtils;
 import lib.TimeWatch;
+import lib.benchmark.EnroneBenchmarkDataLoader;
 import lib.metadata.KVStoreMetaData;
 import lib.metadata.ServerData;
 import org.apache.log4j.LogManager;
@@ -16,16 +17,15 @@ import org.apache.log4j.Logger;
 import java.io.File;
 import java.io.FileWriter;
 import java.nio.file.Paths;
+import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
+import static lib.benchmark.EnroneBenchmarkDataLoader.Loader;
 
 /**
  * This class is only used for getting performance measures for KVClient
@@ -39,8 +39,11 @@ public class KVClientRunner {
 
     static ExecutorService executorService = Executors.newFixedThreadPool(NO_OF_CLIENTS);
 
-    final static File DB_DIRECTORY = new File(Paths.get("stats").toUri());
-    final static File DB_FILE = new File(Paths.get(DB_DIRECTORY.toString(), "stats.csv").toUri());
+    final static File STATS_DIRECTORY = new File(Paths.get("stats").toUri());
+    final static File STATS_FILE = new File(Paths.get(STATS_DIRECTORY.toString(), "stats.csv").toUri());
+
+    final static File TEST_DATA_DIRECTORY = new File(Paths.get("resources/till-beck-140mb").toUri());
+    final static Stream<AbstractMap.SimpleEntry<String, Loader>> testDataStream = EnroneBenchmarkDataLoader.loadData(TEST_DATA_DIRECTORY, true);
 
     enum CommandType {
         GET,
@@ -52,6 +55,16 @@ public class KVClientRunner {
     public static void main(String[] args) {
         runTest();
         executorService.shutdown();
+    }
+
+    private static AbstractMap.SimpleEntry<String, Loader> getKeyValue() {
+        Iterator<AbstractMap.SimpleEntry<String, Loader>> it = testDataStream.iterator();
+
+        if (it.hasNext()) {
+            return it.next();
+        }
+
+        return null;
     }
 
 
@@ -121,7 +134,7 @@ public class KVClientRunner {
             String threadName = Thread.currentThread().getName();
             logger.debug(String.format("The thread %s started running", threadName));
 
-            try (FileWriter writer = new FileWriter(DB_FILE, true)) {
+            try (FileWriter writer = new FileWriter(STATS_FILE, true)) {
 
                 for (int i = 0; i < ROUNDS; i++) {
                     logger.debug(String.format("Running command %d th iteration in thread  %s ", i, threadName));
