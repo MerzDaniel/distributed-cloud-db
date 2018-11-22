f42d6ad13d91f41faab8221d1c90fa265f882310
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-11-22 17:12:02 +0100

    EnroneDataLoader now returns a loader to load the value

--- src/lib/benchmark/EnroneBenchmarkDataLoader.java
+++ src/lib/benchmark/EnroneBenchmarkDataLoader.java
@@ -5,59 +5,67 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class EnroneBenchmarkDataLoader {
     static Logger logger = LogManager.getLogger(EnroneBenchmarkDataLoader.class);
 
-    public static Stream<AbstractMap.SimpleEntry<String, String>> loadData(File file, boolean uniqueKeys) {
+    public static Stream<AbstractMap.SimpleEntry<String, Loader>> loadData(File file, boolean uniqueKeys) {
         return StreamUtils.asStream(createIterator(file, uniqueKeys), false);
     }
 
-    private static Iterator<AbstractMap.SimpleEntry<String, String>> createIterator(File file, boolean uniqueKeys) {
-        return new Iterator<AbstractMap.SimpleEntry<String, String>>() {
+    private static Iterator<AbstractMap.SimpleEntry<String, Loader>> createIterator(File file, boolean uniqueKeys) {
+        return new Iterator<AbstractMap.SimpleEntry<String, Loader>>() {
             List<File> fileList = new LinkedList<>(Arrays.asList(file));
-            AbstractMap.SimpleEntry<String, String> next = null;
+            AbstractMap.SimpleEntry<String, Loader> next = null;
 
             @Override
             public boolean hasNext() {
                 if (next != null) return true;
                 if (fileList.isEmpty()) return false;
 
-                File current = fileList.remove(fileList.size()-1);
+                File current = fileList.remove(fileList.size() - 1);
                 while (current.isDirectory()) {
                     List<File> files = Arrays.asList(current.listFiles());
                     Collections.reverse(files);
                     fileList.addAll(files);
-                    current = fileList.remove(fileList.size()-1);
+                    current = fileList.remove(fileList.size() - 1);
                 }
 
-                try {
-                    String key = uniqueKeys ? current.getAbsolutePath() : current.getName();
-                    String value = new String(Files.readAllBytes(current.toPath()), StandardCharsets.UTF_8);
-                    next = new AbstractMap.SimpleEntry<>(key, value);
-                    return true;
-                } catch (IOException e) {
-                    logger.warn("Error while reading file", e);
-                    return false;
-                }
+                String key = uniqueKeys ? current.getAbsolutePath() : current.getName();
+                next = new AbstractMap.SimpleEntry<>(key, new Loader((current)));
+                return true;
             }
 
             @Override
-            public AbstractMap.SimpleEntry<String, String> next() {
-                AbstractMap.SimpleEntry<String, String> a = next;
+            public AbstractMap.SimpleEntry<String, Loader> next() {
+                AbstractMap.SimpleEntry<String, Loader> a = next;
                 next = null;
                 return a;
             }
         };
     }
 
+    public static class Loader {
+        private File file;
+
+        public Loader(File file) {
+
+            this.file = file;
+        }
+
+        public String Load() {
+            try {
+                return new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                logger.warn("File could not be loaded", e);
+                return "no value...";
+            }
+        }
+    }
 
 }
