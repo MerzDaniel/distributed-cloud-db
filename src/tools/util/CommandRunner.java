0c4dbfe6d7a6e2cac6d8f40464f7e792c0cded9f
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-11-22 17:36:15 +0100

    Remove runner

--- src/tools/util/CommandRunner.java
+++
@@ -1,9 +0,0 @@
-package tools.util;
-
-import client.store.KVStore;
-import lib.metadata.KVStoreMetaData;
-
-public class CommandRunner {
-    private KVStore kvStore = new KVStore(new KVStoreMetaData());
-
-}
