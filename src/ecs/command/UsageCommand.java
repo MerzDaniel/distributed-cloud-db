2b6fe1ec85da7fc5618ecb2d85b37fcf38b3172a
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-11-25 17:38:45 +0100

    Update usage dialog for ecs

--- src/ecs/command/UsageCommand.java
+++ src/ecs/command/UsageCommand.java
@@ -16,10 +16,13 @@ public class UsageCommand implements Command {
     @Override
     public void execute(State state) {
         System.out.println("Usage:");
-        System.out.println("  Add <name> <host> <port> : Add a new Storage server. It will be added to the config.");
-        System.out.println("  check                    : Check state of servers");
-        System.out.println("  start                    : Start the storage servers");
-        System.out.println("  stop <name>              : Stop a server");
+        System.out.println("  Add <name> <host> <port> : [NOT IMPLEMENTED] Add a new Storage server. It will be added to the config.");
+        System.out.println("  status                   : Check state of servers");
+        System.out.println("  startup                  : Startup KvServer processes ( -> UNCONFIGURED state)");
+        System.out.println("  configure                : Configure KvServers ( UNCONFIGURE -> IDLE )");
+        System.out.println("  start                    : Start the storage servers ( IDLE -> RUNNING )");
+        System.out.println("  stop                     : Stop a all servers ( RUNNING -> IDLE )");
+        System.out.println("  shutdown                 : Stop a all server processes");
         System.out.println("  help                     : Show this help");
     }
 }
