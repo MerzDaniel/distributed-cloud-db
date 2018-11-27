1925faba26a807073d2930a64a38829d9a9821ff
Sameera Bandara <sameerahasitha@gmail.com>
2018-11-27 18:13:39 +0100

    modify configure command

--- src/ecs/command/ConfigureAllCommand.java
+++ src/ecs/command/ConfigureAllCommand.java
@@ -2,15 +2,12 @@ package ecs.command;
 
 import ecs.Command;
 import ecs.State;
-import lib.SocketUtil;
-import lib.message.KVAdminMessage;
-import lib.message.KVMessage;
-import lib.message.MessageMarshaller;
+import lib.message.MarshallingException;
 import lib.metadata.ServerData;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.io.IOException;
+
+import static ecs.service.KvService.configure;
 
 /**
  * This class represents  the command for configure all {@link server.KVServer} instances
@@ -28,19 +25,11 @@ public class ConfigureAllCommand implements Command {
 
         for (int index = 0; index < state.storeMeta.getKvServerList().size(); index++) {
             ServerData sd = state.storeMeta.getKvServerList().get(index);
-            try (Socket s = new Socket(sd.getHost(), sd.getPort())) {
-                InputStream i = s.getInputStream();
-                KVMessage connectionSuccessMsg = (KVMessage) MessageMarshaller.unmarshall(SocketUtil.readMessage(i));
 
-                OutputStream o = s.getOutputStream();
-                KVAdminMessage msg = new KVAdminMessage(KVAdminMessage.StatusType.CONFIGURE, state.storeMeta, index);
-                SocketUtil.sendMessage(o, MessageMarshaller.marshall(msg));
-
-                KVAdminMessage response = (KVAdminMessage) MessageMarshaller.unmarshall(SocketUtil.readMessage(i));
-                if (response.status != KVAdminMessage.StatusType.CONFIGURE_SUCCESS) {
-                    System.out.println(String.format("Error while starting server %s:%d : %s", sd.getHost(), sd.getPort(), response.status));
-                }
-            } catch (Exception e) {
+            try {
+                configure(sd, state.storeMeta, index);
+            } catch (IOException | MarshallingException e) {
+                universeIsOk = false;
                 System.out.println(String.format("Error while starting server %s:%d : %s", sd.getHost(), sd.getPort(), e.getMessage()));
             }
         }
