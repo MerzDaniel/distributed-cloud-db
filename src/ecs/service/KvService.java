b27ec3a77b2eee6e72d0cb5254ce614915c9a5d1
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-11-29 11:21:46 +0100

    Replace Socket with Messaging

--- src/ecs/service/KvService.java
+++ src/ecs/service/KvService.java
@@ -12,32 +12,27 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
 
 public final class KvService {
     private static Logger l = LogManager.getLogger(KvService.class);
 
     public static RunningState getStatus(ServerData sd) throws IOException, MarshallingException {
 
-        try (Socket s = new Socket(sd.getHost(), sd.getPort());
-             InputStream i = s.getInputStream();
-             OutputStream o = s.getOutputStream()) {
-            MessageMarshaller.unmarshall(SocketUtil.readMessage(i));
+        Messaging messaging = new Messaging();
+        messaging.connect(sd.getHost(), sd.getPort());
 
-            SocketUtil.sendMessage(o, MessageMarshaller.marshall(new KVAdminMessage(KVAdminMessage.StatusType.STATUS)));
-            KVAdminMessage response = (KVAdminMessage) MessageMarshaller.unmarshall(SocketUtil.readMessage(i));
+        messaging.sendMessage(new KVAdminMessage(KVAdminMessage.StatusType.STATUS));
+        KVAdminMessage response = (KVAdminMessage) messaging.readMessage();
 
-            return response.runningState;
-        }
+        return response.runningState;
 
     }
+
     public static KVAdminMessage moveData(ServerData to, Messaging messaging, boolean softMove) throws IOException, MarshallingException {
         KVAdminMessage.StatusType statusType = softMove ? KVAdminMessage.StatusType.MOVE_SOFT : KVAdminMessage.StatusType.MOVE;
         KVAdminMessage msg = new KVAdminMessage(statusType, to);
         messaging.sendMessage(msg);
-        return (KVAdminMessage)messaging.readMessage();
+        return (KVAdminMessage) messaging.readMessage();
     }
 
     public static boolean makeReadonly(ServerData sd, Messaging messaging) throws MarshallingException, IOException {
