3e8349c695e473dce5b04863836fd8f7421212ab
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-11-29 17:53:23 +0100

    Add script for stripping log files to a smaller size

---
+++ src/tools/StripLogFiles.java
@@ -0,0 +1,27 @@
+package tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class StripLogFiles {
+    final static int TARGET_FILE_SIZE = 1024 * 1024 * 2;
+
+    public static void main(String[] args) throws IOException {
+        String files[] = {"client.log", "ecs.log", "server.log", "tools.log"};
+        for (String f : files) {
+            File file = new File(Paths.get("C:\\Users\\daniel\\git\\gr10\\logs", f).toString());
+//            r.seek(r.length() - 10000000);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.skip(file.length() - TARGET_FILE_SIZE);
+            byte buf[] = new byte[TARGET_FILE_SIZE];
+            fileInputStream.read(buf);
+            String s = new String(buf);
+            fileInputStream.close();
+
+            new FileWriter(file).write(s);
+        }
+    }
+}
