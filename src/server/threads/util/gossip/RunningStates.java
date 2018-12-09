df18f8e2806e25f5ab4180fd347687f539404e7e
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-12-09 12:46:38 +0100

    GossipStatusThread: Extract combineInto method

--- src/server/threads/util/gossip/RunningStates.java
+++ src/server/threads/util/gossip/RunningStates.java
@@ -9,21 +9,26 @@ public class RunningStates {
 
     public static TimedRunningStateMap combineMaps(TimedRunningStateMap map0, TimedRunningStateMap map1) {
         TimedRunningStateMap result = new TimedRunningStateMap();
+
+        combineMapsInto(result, map0, map1);
+
+        return result;
+    }
+
+    public static void combineMapsInto(TimedRunningStateMap targetMap, TimedRunningStateMap map0, TimedRunningStateMap map1) {
         List<String> checkedServers = new LinkedList<>();
         for (String key: map0.getKeys()) {
             checkedServers.add(key);
             if (!map1.hasKey(key))
-                result.put(key, map0.get(key));
+                targetMap.put(key, map0.get(key));
             else if (map0.get(key).accessTime >= map1.get(key).accessTime)
-                result.put(key, map0.get(key));
+                targetMap.put(key, map0.get(key));
             else
-                result.put(key, map1.get(key));
+                targetMap.put(key, map1.get(key));
         }
         for (String key: map1.getKeys()) {
             if (checkedServers.contains(key)) continue;
-            result.put(key, map1.get(key));
+            targetMap.put(key, map1.get(key));
         }
-
-        return result;
     }
 }
