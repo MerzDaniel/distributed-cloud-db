5da7be90b530897f425fe7f92e7a992d9347d4e5
Sameera Bandara <sameerahasitha@gmail.com>
2018-12-11 20:27:18 +0100

    add logic to send put messages to replicas

--- src/server/threads/handler/PutHandler.java
+++ src/server/threads/handler/PutHandler.java
@@ -1,12 +1,17 @@
 package server.threads.handler;
 
-import lib.message.KVMessage;
-import lib.message.MessageFactory;
+import lib.message.*;
+import lib.metadata.KVServerNotFoundException;
+import lib.metadata.ServerData;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import server.IMessageHandler;
 import server.ServerState;
 import server.kv.DbError;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class PutHandler implements IMessageHandler {
     Logger logger = LogManager.getLogger(this.getClass().getName());
@@ -32,10 +37,51 @@ public class PutHandler implements IMessageHandler {
                 response = MessageFactory.createPutErrorMessage();
             }
         }
+
+        if(MessageHandlerUtils.isResponsibleCoordinator(state, request.getKey())) {
+            this.sendToReplicas(request, state);
+        }
+
         return response;
     }
 
     private boolean shouldDelete(String value) {
         return value == null || value.equals("") || value.equals("null");
     }
+
+    private KVAdminMessage sendToReplica(KVMessage request, ServerData serverData) {
+        Messaging con;
+        KVAdminMessage response;
+
+        con = new Messaging();
+        try {
+            con.connect(serverData.getHost(), serverData.getPort());
+            KVAdminMessage msg = new KVAdminMessage(KVAdminMessage.StatusType.PUT_REPLICATE, request.getKey(), request.getValue());
+            con.sendMessage(msg);
+            response = (KVAdminMessage) con.readMessage();
+        } catch (IOException | MarshallingException e) {
+            logger.warn("An error occurred while replicating data ", e);
+            return new KVAdminMessage(KVAdminMessage.StatusType.PUT_REPLICATE_ERROR);
+        }
+
+        return response;
+    }
+
+    private KVAdminMessage sendToReplicas(KVMessage request, ServerState state) {
+        List<ServerData> replicaServers;
+
+        try {
+            replicaServers = state.meta.findReplicaKVServers(request.getKey());
+        } catch (KVServerNotFoundException e) {
+            return new KVAdminMessage(KVAdminMessage.StatusType.PUT_REPLICATE_ERROR);
+        }
+
+        for (ServerData serverData : replicaServers) {
+            CompletableFuture.runAsync(() -> {
+                this.sendToReplica(request, serverData);
+            });
+        }
+        return new KVAdminMessage(KVAdminMessage.StatusType.PUT_REPLICATE_SUCCESS);
+    }
 }
+
