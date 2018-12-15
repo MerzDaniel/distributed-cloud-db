33e1c6524943b1e7b2e3d85ed0457d32f06ae6be
Sameera Bandara <sameerahasitha@gmail.com>
2018-12-15 13:38:06 +0100

    modify PutHandler

--- src/server/threads/handler/PutHandler.java
+++ src/server/threads/handler/PutHandler.java
@@ -20,22 +20,32 @@ public class PutHandler implements IMessageHandler {
     @Override
     public KVMessage handleRequest(KVMessage request, ServerState state) {
         KVMessage response;
-        KeyValueStore db = MessageHandlerUtils.getDatabase(state, request.getKey());
+
+        KeyValueStore db;
+
         if (shouldDelete(request.getValue())) {
             try {
+                db = MessageHandlerUtils.getDatabase(state, request.getKey());
                 db.deleteKey(request.getKey());
                 response = MessageFactory.createDeleteSuccessMessage();
             } catch (DbError dbError) {
                 logger.warn("PUT: Databaseerror while deleting a value", dbError);
                 response = MessageFactory.createDeleteErrorMessage();
+            } catch (NoKeyValueStoreException e) {
+                logger.warn(String.format("Couldn't find the responsible database for key '%s'", request.getKey()));
+                response = MessageFactory.createDeleteErrorMessage();
             }
         } else {
             try {
+                db = MessageHandlerUtils.getDatabase(state, request.getKey());
                 boolean updated = db.put(request.getKey(), request.getValue());
                 if (updated) response = MessageFactory.createPutUpdateMessage();
                 else response = MessageFactory.createPutSuccessMessage();
             } catch (DbError dbError) {
                 logger.warn("PUT: Databaseerror while PUT a value", dbError);
+                response = MessageFactory.createPutErrorMessage();
+            } catch (NoKeyValueStoreException e) {
+                logger.warn(String.format("Couldn't find the responsible database for key '%s'", request.getKey()));
                 response = MessageFactory.createPutErrorMessage();
             }
         }
