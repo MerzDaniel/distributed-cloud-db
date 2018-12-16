291d4ee26c72c9edbf867f57055c538947d15f7d
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-12-16 15:43:52 +0100

    DbProvider: Remove old replicaMap

--- src/server/ServerState.java
+++ src/server/ServerState.java
@@ -6,19 +6,14 @@ import lib.server.RunningState;
 import lib.server.TimedRunningStateMap;
 import server.kv.DbProvider;
 import server.kv.KeyValueStore;
-import server.kv.RandomAccessKeyValueStore;
 import server.threads.AbstractServerThread;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class ServerState {
     public KVStoreMetaData meta = new KVStoreMetaData();
     public DbProvider dbProvider;
-    private Map<String, KeyValueStore> replicaMap;
     public ServerData currentServerServerData;
     public RunningState runningState = RunningState.UNCONFIGURED;
     public TimedRunningStateMap stateOfAllServers = new TimedRunningStateMap();
@@ -29,7 +24,6 @@ public class ServerState {
      */
     public ServerState(KeyValueStore db, ServerData serverData) {
         this.dbProvider = new DbProvider(serverData, db);
-        this.replicaMap = new HashMap<>();
         this.currentServerServerData = serverData;
     }
 
@@ -38,16 +32,6 @@ public class ServerState {
      */
     public ServerState(ServerData serverData) {
         this.currentServerServerData = serverData;
-    }
-
-    public KeyValueStore getReplica(String serverName) throws IOException {
-        if (!replicaMap.containsKey(serverName)) {
-            KeyValueStore kvStore = new RandomAccessKeyValueStore();
-            kvStore.init("replica<" + serverName + ">_" + currentServerServerData.getName());
-            replicaMap.put(serverName, kvStore);
-        }
-
-        return replicaMap.get(serverName);
     }
 
     public void init(ServerData serverData) {
