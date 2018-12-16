cca72f827c3f278fc7a2549b0efee73710e6a55f
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-12-16 15:02:33 +0100

    DbProvider: Impl CreateDb for a ServerData obj

--- src/server/kv/DbProvider.java
+++ src/server/kv/DbProvider.java
@@ -1,16 +1,23 @@
 package server.kv;
 
 import lib.metadata.ServerData;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import server.kv.cache.FifoCachedKeyValueStore;
+import server.kv.cache.LFUCachedKeyValueStore;
+import server.kv.cache.LRUCachedKeyValueStore;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class DbProvider {
     HashMap<String, KeyValueStore> dbMap = new HashMap<>();
-
+    private ServerData coordinator;
+    private Logger logger = LogManager.getLogger(DbProvider.class);
 
     public DbProvider(ServerData coordinator) {
-
+        this.coordinator = coordinator;
     }
 
     /**
@@ -24,10 +31,41 @@ public class DbProvider {
      * Get database for a specific ServerData
      */
     public KeyValueStore getDb(ServerData server) {
-        throw new NotImplementedException();
+        String serverName = server.getName();
+        if (dbMap.get(serverName) != null) return dbMap.get(serverName);
+
+        return createNewDb(server);
     }
+
 
     public void shutdown() {
 
+    }
+
+    private KeyValueStore createNewDb(ServerData server) {
+        KeyValueStore db = new RandomAccessKeyValueStore();
+        try {
+            db.init(server.getName());
+        } catch (IOException e) {
+            logger.warn("Could not create database");
+            return null;
+        }
+        switch (coordinator.getCacheType()) {
+            case FIFO:
+                logger.info("Setting up FIFO caching");
+                db = new FifoCachedKeyValueStore(coordinator.getCacheSize(), db);
+                break;
+            case LRU:
+                logger.info("Setting up LRU caching");
+                db = new LRUCachedKeyValueStore(coordinator.getCacheSize(), db);
+                break;
+            case LFU:
+                logger.info("Setting up LFU caching");
+                db = new LFUCachedKeyValueStore(coordinator.getCacheSize(), db);
+                break;
+        }
+
+        dbMap.put(server.getName(), db);
+        return db;
     }
 }
