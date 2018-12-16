fffc583e48bd71ade277c36fcdcc8e3f15c90f29
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-12-16 16:33:48 +0100

    ReplicationTest: Add a MemoryDatabase for testing purposes

---
+++ src/server/kv/MemoryDatabase.java
@@ -0,0 +1,47 @@
+package server.kv;
+
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.stream.Stream;
+
+public class MemoryDatabase implements KeyValueStore {
+    HashMap<String, String> db = new HashMap<>();
+
+    @Override
+    public void init(String dbName) throws IOException {
+        db.clear();
+    }
+
+    @Override
+    public void shutdown() throws IOException {
+        db.clear();
+    }
+
+    @Override
+    public String get(String key) throws KeyNotFoundException, DbError {
+        return db.get(key);
+    }
+
+    @Override
+    public boolean put(String key, String value) throws DbError {
+        return db.put(key, value) != null;
+    }
+
+    @Override
+    public boolean hasKey(String key) throws DbError {
+        return db.get(key) != null;
+    }
+
+    @Override
+    public boolean deleteKey(String key) throws DbError {
+        return db.remove(key) != null;
+    }
+
+    @Override
+    public Stream<AbstractMap.SimpleEntry<String, String>> retrieveAllData() {
+        return db.entrySet().stream().map(e -> new AbstractMap.SimpleEntry<>(e.getKey(), e.getValue()));
+    }
+}
