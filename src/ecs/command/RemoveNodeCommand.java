b8cecba6906b6fcc538f23ce3ead23e15be564a3
Sameera Bandara <sameerahasitha@gmail.com>
2018-12-17 22:47:23 +0100

    modify remove node command

--- src/ecs/command/RemoveNodeCommand.java
+++ src/ecs/command/RemoveNodeCommand.java
@@ -2,9 +2,9 @@ package ecs.command;
 
 import ecs.Command;
 import ecs.State;
-import lib.TimeWatch;
-import lib.message.KVAdminMessage;
-import lib.message.Messaging;
+import ecs.service.KvService;
+import lib.message.MarshallingException;
+
 import lib.metadata.KVServerNotFoundException;
 import lib.metadata.ServerData;
 import org.apache.log4j.LogManager;
@@ -13,12 +13,12 @@ import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.util.Collections;
 
-import static ecs.service.KvService.moveData;
 
 /**
  * This class represent the command for removing a {@link server.KVServer} instance
  */
 public class RemoveNodeCommand implements Command {
+
     private Logger logger = LogManager.getLogger(RemoveNodeCommand.class);
 
     /**
@@ -41,57 +41,15 @@ public class RemoveNodeCommand implements Command {
         }
 
         Collections.shuffle(state.storeMeta.getKvServerList());
-        ServerData from = state.storeMeta.getKvServerList().remove(0);
-
-        ServerData to = null;
-        try {
-            to = state.storeMeta.findNextKvServerByHash(from.getFromHash());
-        } catch (KVServerNotFoundException e) {
-            logger.error("Error.", e);
-            System.out.println("Error while removing the server.");
-        }
-
-        if (from == null || to == null) {
-            System.out.println("Server does not exist and can therefor not be removed.");
-            return;
-        }
-
-        Messaging con = new Messaging();
-        try {
-            con.connect(from.getHost(), from.getPort());
-        } catch (IOException e) {
-            logger.warn("Error.", e);
-            System.out.println("Error while connecting to the server.");
-            return;
-        }
+        ServerData removedNode = state.storeMeta.getKvServerList().get(0);
 
         try {
-            // move data
-            TimeWatch moveTimer = TimeWatch.start();
-            KVAdminMessage response = moveData(to, con, false);
-            String responseString;
-            if (response.status != KVAdminMessage.StatusType.MOVE_SUCCESS)
-                System.out.println("Data was not successfully moved. But I'm evil and still shutting down the node ^_^");
-            System.out.format("Move data took %dms.\n", moveTimer.time());
-
-            // reconfigure
-            state.storeMeta.getKvServerList().remove(from);
-            new ConfigureAllCommand().execute(state);
-
-            // stop
-            KVAdminMessage stopMsg = new KVAdminMessage(KVAdminMessage.StatusType.SHUT_DOWN);
-            con.sendMessage(stopMsg);
-            response = (KVAdminMessage) con.readMessage();
-            if (response.status != KVAdminMessage.StatusType.SHUT_DOWN_SUCCESS)
-                System.out.println("Was not successfully shutdown");
-            else
-                System.out.println("Successfully shutdown the node");
-
-        } catch (Exception e) {
-            logger.warn("Error.", e);
-            System.out.println("Error during removing the server and moving the data");
-            return;
+            KvService.removeNode(removedNode, state);
+        } catch (KVServerNotFoundException | IOException | MarshallingException e) {
+            logger.error("There was an error while re-configure the system", e);
+            System.out.println("There was an error while re-configure the system");
         }
     }
+
 
 }
