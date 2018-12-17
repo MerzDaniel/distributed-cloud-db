ed8946e5aec4998f79b295f7574fe1a2b061bfbe
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-12-17 23:50:45 +0100

    ECS: Add failover command

---
+++ src/ecs/command/FailOverCommand.java
@@ -0,0 +1,54 @@
+package ecs.command;
+
+import ecs.Command;
+import ecs.State;
+import ecs.service.KvService;
+import lib.message.MarshallingException;
+import lib.metadata.KVServerNotFoundException;
+import lib.metadata.ServerData;
+import lib.server.RunningState;
+import lib.server.TimedRunningState;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FailOverCommand implements Command {
+    @Override
+    public void execute(State state) {
+        List<ServerData> failedServers = new LinkedList<>();
+        try {
+            KvService.gossipServers(state.poolMeta.getKvServerList(), state.timedRunningStateMap);
+            failedServers = new LinkedList<>();
+            for (ServerData sd : state.poolMeta.getKvServerList()) {
+                TimedRunningState timedRunningState = state.timedRunningStateMap.get(sd.getName());
+                if (timedRunningState.runningState == RunningState.DOWN)
+                    failedServers.add(sd);
+            }
+
+            if (failedServers.size() == 0) {
+                System.out.println("The system seems to be running fine!");
+                return;
+            }
+
+            if (failedServers.size() > 1) {
+                System.out.println("Two many servers failed!!!" );
+                System.out.println("Data cannot be recovered, please set everything up again, manually.");
+                return;
+            }
+        } catch (MarshallingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Oh no, one server failed! Will try to recover the data and ensure full replication!");
+        try {
+            KvService.removeNode(failedServers.get(0),state.poolMeta);
+        } catch (Exception e) {
+            System.out.println("Some error occured while recovering stuff.... ");
+            System.out.println(e.getMessage());
+            return;
+        }
+        System.out.println("Successfully ensured full replication again :)");
+    }
+}
