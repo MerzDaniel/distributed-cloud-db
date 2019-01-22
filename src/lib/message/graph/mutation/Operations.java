3922da7cb795a9637173eab9c3d2da34070849b7
Daniel Merz <MerzDanielDeveloper@gmail.com>
2019-01-22 22:27:23 +0100

    Mutation: Document mutationTypes

--- src/lib/message/graph/mutation/Operations.java
+++ src/lib/message/graph/mutation/Operations.java
@@ -1,7 +1,16 @@
 package lib.message.graph.mutation;
 
 public enum Operations {
+    /**
+     * Replace a propterty in a document
+     */
     REPLACE,
+    /**
+     * Merge values of a property in a document (for example add values to an array)
+     */
     MERGE,
+    /**
+     * Used for data mutation in a nested data structure
+     */
     NESTED,
 }
