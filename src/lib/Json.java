d3a9caa5153334697d884c73a708107fa780e247
Sameera Bandara <sameerahasitha@gmail.com>
2019-01-22 23:22:55 +0100

    add getProperty method

--- src/lib/Json.java
+++ src/lib/Json.java
@@ -51,4 +51,8 @@ public class Json {
     public void addProperty(Property property){
         properties.add(property);
     }
+
+    public Property getProperty(String key) {
+        return properties.stream().filter(it -> it.key.equals(key)).findFirst().get();
+    }
 }
