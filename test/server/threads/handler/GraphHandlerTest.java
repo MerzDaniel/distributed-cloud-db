113b09e2a5bfcfe69ac136c0ad8f2b6664969579
Sameera Bandara <sameerahasitha@gmail.com>
2019-01-25 17:28:10 +0100

    tests for json object merge scenarios

--- test/server/threads/handler/GraphHandlerTest.java
+++ test/server/threads/handler/GraphHandlerTest.java
@@ -2,8 +2,10 @@ package server.threads.handler;
 
 import lib.json.Json;
 import lib.message.exception.MarshallingException;
+import lib.message.exception.UnsupportedJsonStructureFoundException;
 import lib.message.graph.GraphDbMessage;
 import lib.message.graph.mutation.MutationMessageImpl;
+import lib.message.graph.response.ResponseMessageImpl;
 import lib.message.kv.KVMessage;
 import lib.message.kv.KvMessageFactory;
 import lib.metadata.KVServerNotFoundException;
@@ -31,18 +33,28 @@ public class GraphHandlerTest {
     final String newPropKey = "newPropKey";
     final String newPropVal = "newPropVal";
 
+    //json with inner json object
+    final String docIdWithInnerJson = "docIdWithInnerJson";
+    final String innerJsonPropKey = "innerJsonPropKey";
+    final String innerJsonPropVal = "innerJsonPropVal";
 
     @Before
     public void setup() {
         state = TestServerState.create();
-        Json doc = Json.Builder.create().withStringProperty(propKey, propVal).finish();
+        Json doc1 = Json.Builder.create().withStringProperty(propKey, propVal).finish();
 
-        KVMessage putMsg = KvMessageFactory.createPutMessage(docId, doc.serialize());
-        new PutHandler().handleRequest(putMsg, state);
+        KVMessage putMsg1 = KvMessageFactory.createPutMessage(docId, doc1.serialize());
+        new PutHandler().handleRequest(putMsg1, state);
+
+        //setup json data with inner json
+        Json doc2 = Json.Builder.create().withJsonProperty(propKey, Json.Builder.create().withStringProperty(innerJsonPropKey, innerJsonPropVal).finish()).finish();
+
+        KVMessage putMsg2 = KvMessageFactory.createPutMessage(docIdWithInnerJson, doc2.serialize());
+        new PutHandler().handleRequest(putMsg2, state);
     }
 
     @Test
-    public void writeNewProp() throws MarshallingException, IOException, DbError, KVServerNotFoundException, KeyNotFoundException {
+    public void writeNewProp() throws MarshallingException, IOException, DbError, KVServerNotFoundException, KeyNotFoundException, UnsupportedJsonStructureFoundException {
         GraphDbMessage mutationMsg = MutationMessageImpl.Builder.create(docId).withReplace(
                 newPropKey,
                 new Json.StringValue(newPropVal)).finish();
@@ -56,7 +68,7 @@ public class GraphHandlerTest {
     }
 
     @Test
-    public void replaceExistingProp() throws MarshallingException, IOException, DbError, KVServerNotFoundException, KeyNotFoundException {
+    public void replaceExistingProp() throws MarshallingException, IOException, DbError, KVServerNotFoundException, KeyNotFoundException, UnsupportedJsonStructureFoundException {
         GraphDbMessage mutationMsg = MutationMessageImpl.Builder.create(docId).withReplace(
                 propKey,
                 new Json.StringValue(newPropVal)).finish();
@@ -71,7 +83,7 @@ public class GraphHandlerTest {
     }
 
     @Test
-    public void writeToNonExistingDoc() throws MarshallingException, IOException, DbError, KVServerNotFoundException, KeyNotFoundException {
+    public void writeToNonExistingDoc() throws MarshallingException, IOException, DbError, KVServerNotFoundException, KeyNotFoundException, UnsupportedJsonStructureFoundException {
         final String nonExistDocId = "nonExisitingDocId";
         GraphDbMessage mutationMsg = MutationMessageImpl.Builder.create(nonExistDocId).withReplace(
                 propKey,
@@ -86,7 +98,42 @@ public class GraphHandlerTest {
 
     }
 
-    // TODO GRAPH: Write tests for MERGE properties (merge json and arrays)
+    @Test
+    public void testMergeJson() throws MarshallingException, IOException, UnsupportedJsonStructureFoundException, DbError, KVServerNotFoundException, KeyNotFoundException {
+        Json propVal = Json.Builder.create().withStringProperty("key001", "val001").finish();
+        GraphDbMessage mutationMsg = MutationMessageImpl.Builder.create(docIdWithInnerJson).withMerge(
+                propKey,
+                new Json.JsonValue(propVal)).finish();
+        GraphMessageHandler.handle(mutationMsg,state);
 
-    //TODO GRAPH: if the document is not a valid json throw an error
+        KVMessage response = new GetHandler().handleRequest(KvMessageFactory.createGetMessage(docIdWithInnerJson), state);
+        Json newDoc = Json.deserialize(response.getValue());
+        assertEquals(String.format("{%s:%s,key001,val001", innerJsonPropKey, innerJsonPropVal), newDoc.get(propKey).serialize());
+
+    }
+
+    @Test
+    public void testMergePropForInvalidDocument() throws MarshallingException, IOException, UnsupportedJsonStructureFoundException, DbError, KVServerNotFoundException, KeyNotFoundException {
+        Json propVal = Json.Builder.create().withStringProperty("key001", "val001").finish();
+
+        //the property of the document which is going to be merged is type of StringValue
+        GraphDbMessage mutationMsg = MutationMessageImpl.Builder.create(docId).withMerge(
+                propKey,
+                new Json.JsonValue(propVal)).finish();
+        ResponseMessageImpl iMessage = (ResponseMessageImpl) GraphMessageHandler.handle(mutationMsg,state);
+        assertEquals("Unsupported structure of the message/document to perform the operation", iMessage.errorMsg);
+    }
+
+    @Test
+    public void testMergePropForInvalidMessage() throws MarshallingException, IOException, UnsupportedJsonStructureFoundException, DbError, KVServerNotFoundException, KeyNotFoundException {
+
+        //the property of the message which is going to be merged is type of StringValue
+        GraphDbMessage mutationMsg = MutationMessageImpl.Builder.create(docIdWithInnerJson).withMerge(
+                propKey,
+                new Json.StringValue("somevalue")).finish();
+        ResponseMessageImpl iMessage = (ResponseMessageImpl) GraphMessageHandler.handle(mutationMsg,state);
+        assertEquals("Unsupported structure of the message/document to perform the operation", iMessage.errorMsg);
+    }
+
+    // TODO GRAPH: Write tests for MERGE properties (for arrays)
 }
