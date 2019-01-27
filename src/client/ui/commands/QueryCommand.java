ffb35d4506b385588197f58ec8bbc59474105b14
Sameera Bandara <sameerahasitha@gmail.com>
2019-01-27 11:38:43 +0100

    support a general query "query <documentId> <prop1, prop2>"

--- src/client/ui/commands/QueryCommand.java
+++ src/client/ui/commands/QueryCommand.java
@@ -11,7 +11,6 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 import static client.ui.Util.writeLine;
@@ -20,11 +19,6 @@ import static client.ui.Util.writeLine;
  * Issues a Query on the server
  */
 public class QueryCommand implements Command {
-
-    private static final String QUERY = "query";
-    private static final String QUERY_BY_USER = "queryByUser";
-    private static final String QUERY_BY_MESSAGE = "queryByMessage";
-
     private String queryParam;
     private List<String> queryProps;
 
@@ -37,13 +31,6 @@ public class QueryCommand implements Command {
 
     @Override
     public void execute(ApplicationState state) {
-
-        if (!Arrays.asList(QUERY, QUERY_BY_MESSAGE, QUERY_BY_USER).contains(queryParam)) {
-            logger.error("unsupported query type");
-            writeLine("Unrecognized query type : " + queryParam);
-            new HelpCommand().execute(state);
-            return;
-        }
 
         ResponseMessageImpl graphMessageResponse = null;
         TimeWatch t = TimeWatch.start();
