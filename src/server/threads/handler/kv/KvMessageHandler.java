a843d3179cd7a33838f2d25ab797abf2bd48fc96
Daniel Merz <MerzDanielDeveloper@gmail.com>
2019-01-28 18:51:17 +0100

    Move Server is in READONLY state checks to ConnectionHandler.java

--- src/server/threads/handler/kv/KvMessageHandler.java
+++ src/server/threads/handler/kv/KvMessageHandler.java
@@ -2,13 +2,10 @@ package server.threads.handler.kv;
 
 import lib.message.kv.KVMessage;
 import lib.message.kv.KvMessageFactory;
-import lib.server.RunningState;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import server.ServerState;
 import server.threads.handler.MessageHandlerUtils;
-
-import java.util.Arrays;
 
 import static lib.message.MessageUtil.isValidKey;
 import static lib.message.MessageUtil.isValidValue;
@@ -17,23 +14,9 @@ public final class KvMessageHandler {
     static Logger logger = LogManager.getLogger(KvMessageHandler.class);
 
     public static KVMessage handleKvMessage(KVMessage kvMessage, ServerState state) {
-        if (
-                !Arrays.asList(RunningState.RUNNING, RunningState.READONLY).contains(state.runningState)) {
-            logger.info(String.format("Client issued %s while server is in state %s",
-                    kvMessage.getStatus().toString(), state.runningState.toString())
-            );
-            return KvMessageFactory.creatServerStopped();
-        }
 
         if (!MessageHandlerUtils.isResponsible(state, kvMessage.getKey(), kvMessage.getStatus())) {
             return KvMessageFactory.createServerNotResponsibleMessage(kvMessage.getKey(), state.meta.marshall());
-        }
-
-        if (state.runningState == RunningState.READONLY && kvMessage.getStatus() != KVMessage.StatusType.GET) {
-            logger.info(String.format("Client issued %s while server is in state %s",
-                    kvMessage.getStatus().toString(), state.runningState.toString())
-            );
-            return KvMessageFactory.createServerWriteLock();
         }
 
         if (!isValidKeyValueLength(kvMessage)) {
