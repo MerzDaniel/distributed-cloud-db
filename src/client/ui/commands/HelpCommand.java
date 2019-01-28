95830aef88e36d00d76c4b46d61434d18f54e3c7
Daniel Merz <MerzDanielDeveloper@gmail.com>
2019-01-28 23:49:45 +0100

    Update help command

--- src/client/ui/commands/HelpCommand.java
+++ src/client/ui/commands/HelpCommand.java
@@ -10,17 +10,18 @@ public class HelpCommand implements Command {
     @Override
     public void execute(ApplicationState state) {
         System.out.println("Usage:");
-        System.out.println("connect <host> <port> : Connect to a host");
-        System.out.println("put <key> <value>     : Put <key,value> to the data store. <key> should not have any whitespaces");
-        System.out.println("get <key>             : Get <value> for the <key> from the data store");
-        System.out.println("query <documentId> <property1>,<property2>, ...         " +
+        System.out.println("connect <host> <port>                   : Connect to a host");
+        System.out.println("put <key> <value>                       : Put <key,value> to the data store. <key> should not have any whitespaces");
+        System.out.println("get <key>                               : Get <value> for the <key> from the data store");
+        System.out.println("query <documentId> <property1>,<property2>, ...                        " +
                                         ": Get the result <property1>,<property2>, ... for document of <documentId>");
-        System.out.println("mutate <documentId> <property1>:<value1>,<property2>:<value2>, ...         " +
+        System.out.println("query <docId> <propWhichIsRefToOtherDoc>|FOLLOW{<propInOtherDoc>}      : follows reference into other document");
+        System.out.println("mutate <documentId> <property1>:<value1>,<property2>:<value2>, ...     " +
                 ": Create a new document {property1:value1,property2:value2,...} with id <documentId>");
-        System.out.println("logLevel <level>      : Set loglevel. Supported logLevels are ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF");
-        System.out.println("disconnect            : Disconnects from the connected host");
-        System.out.println("c                     : Fast connect to localhost:50000");
-        System.out.println("help                  : Print this help text");
-        System.out.println("quit                  : Exit application");
+        System.out.println("logLevel <level>                        : Set loglevel. Supported logLevels are ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF");
+        System.out.println("disconnect                              : Disconnects from the connected host");
+        System.out.println("c                                       : Fast connect to localhost:50000");
+        System.out.println("help                                    : Print this help text");
+        System.out.println("quit                                    : Exit application");
     }
 }
